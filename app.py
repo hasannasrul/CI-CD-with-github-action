5475fd4dca5ffd159d67b302e8d412674d85f50d
hasannasrul <hasanali242424@gmail.com>
2023-09-14 22:59:16 +0530

    added var url

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return {
+    return jsonify({
         "status": 200,
         "data": "flask code"
-    }
+    })
+
+@app.route('/api/login/<name>')
+def login(name):
+    return jsonify({
+        "status": 200,
+        "data": name
+    })
 
 if __name__ =='__main__':
     app.run('0.0.0.0')
