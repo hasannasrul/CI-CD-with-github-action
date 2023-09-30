88625a6050dd19f01d6765ad146a404b575637be
hasannasrul <hasanali242424@gmail.com>
2023-09-30 20:09:13 +0530

    added comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
 }
-
+# create a ec2 instance
 resource "aws_instance" "webserver" {
     ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
@@ -13,22 +13,22 @@ resource "aws_instance" "webserver" {
     user_data = file("../scripts/env.sh")
       
 }
-
+# variable to use in security group for ingress
 variable "ingress" {
   type        = list(number)
   default     = [80,443,22,3000]
   description = "security group ports"
 }
-
+# variable to use in security group for egress
 variable "egress" {
   type        = list(number)
   default     = [80,443]
   description = "security group ports"
 }
-
+# Create security group resource
 resource "aws_security_group" "sg" {
     name = "Allow web traffic"
-
+    # create multiple ingress blocks with help of iterators
     dynamic "ingress" {
         iterator = port
         for_each = var.ingress
@@ -39,7 +39,7 @@ resource "aws_security_group" "sg" {
             cidr_blocks = ["0.0.0.0/0"] 
         }
     }
-
+    # create multiple egress blocks with help of iterators
     dynamic "egress" {
         iterator = port
         for_each = var.egress
@@ -51,8 +51,8 @@ resource "aws_security_group" "sg" {
         }
     }
 }
-
+#output public ip
 output ec2-id {
-  value       = aws_instance.webserver.id
-  description = "ec2 instance id"
+  value       = aws_instance.webserver.public_ip
+  description = "ec2 instance public ip"
 }
